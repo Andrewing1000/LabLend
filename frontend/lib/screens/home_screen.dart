4ae57eada1ccd1a5bbe6edb7ec24ab4028fe59da
Mr Pedro Santos <147351155+nohpedro@users.noreply.github.com>
2024-05-19 15:31:17 -0400

    Agregar icono
    jeje perdon andres trabaje aqui

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -4,7 +4,8 @@ import 'package:frontend/widgets/card_section.dart';
 import 'package:frontend/widgets/horizontal_card.dart';
 import '../widgets/navbar.dart';
 import '../widgets/horizontal_section.dart'; // Asegúrate de que los widgets de tarjetas están aquí
-
+import '../widgets/accion_boton.dart'; // Importa el widget de acción del botón
+import '../widgets/notification.dart'; // Importa el widget de notificación
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,36 +14,35 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ScrollController _scrollController = ScrollController();
-  final List<NavItem> items = [
+  bool _showNotification = false; // Estado para controlar la visibilidad de la notificación
+  String _notificationMessage = ''; // Estado para almacenar el mensaje de la notificación
 
+  final List<NavItem> items = [
     NavItem(
         iconNormal: Icons.home_outlined,
         iconSelected: Icons.home,
-        onPressed: (){
+        onPressed: () {
           print("Opcion1");
         },
         title: "Home"),
-
     NavItem(
-        iconNormal : Icons.search_sharp,
+        iconNormal: Icons.search_sharp,
         iconSelected: Icons.search,
-        onPressed: (){
+        onPressed: () {
           print("Opcion2");
         },
         title: "Search"),
-
     NavItem(
-        iconNormal : Icons.notifications_none_outlined,
+        iconNormal: Icons.notifications_none_outlined,
         iconSelected: Icons.notifications,
-        onPressed: (){
+        onPressed: () {
           print("Opcion3");
         },
         title: "Notifications"),
-
     NavItem(
-        iconNormal : Icons.person_2_outlined,
+        iconNormal: Icons.person_2_outlined,
         iconSelected: Icons.person_2,
-        onPressed: (){
+        onPressed: () {
           print("Opcion4");
         },
         title: "Profile"),
@@ -59,47 +59,112 @@ class _HomeScreenState extends State<HomeScreen> {
     HorizontalCard(title: "Opcion4"),
   ];
 
-
   List<CustomCard> cItems = [
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-    CustomCard(title: "Opcion1", subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',),
-
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnflksflk sdjnflksdjf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf lksflk sdjnflksdjf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf lksflk sdjnflksdjf',
+    ),
+    CustomCard(
+      title: "Opcion1",
+      subtitle: 'dskjafnsdlkjfnlsdj dsfslakdjnflsajdnf lksflksdjnf lksflk sdjnflksdjf',
+    ),
   ];
+
+  // Método para alternar la visibilidad de la notificación y establecer el mensaje
+  void _toggleNotification(String message) {
+    setState(() {
+      _notificationMessage = message; // Establecer el mensaje de la notificación
+      _showNotification = true; // Mostrar la notificación
+    });
+
+    // Ocultar la notificación automáticamente después de 3 segundos
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        _showNotification = false; // Ocultar la notificación
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         color: Colors.black,
-        child: Row(
+        child: Stack(
           children: [
-              VerticalNavbar(
-                items: items,
-                iconSize: 40,
-              ),
-            Expanded(
-              child: SingleChildScrollView(
-                controller: _scrollController,
-                child: Column(
-                  children: [
-                    HorizontalCardSection(items: hcItems,), // Sección nueva
-                    CardSection(items : cItems),
-                    CardSection(items : cItems),
-                    CardSection(items : cItems),
-                    CardSection(items : cItems),
-                    CardSection(items : cItems),
-                  ],
+            Row(
+              children: [
+                VerticalNavbar(
+                  items: items,
+                  iconSize: 40,
                 ),
-              ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    controller: _scrollController,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // Botón de acción con icono de "+"
+                              AccionBoton(
+                                onNotification: _toggleNotification,
+                                message: "¡agregado!",
+                                icon: Icons.add,
+                              ),
+                              SizedBox(width: 20),
+                              // Botón de acción con icono de "-"
+                              AccionBoton(
+                                onNotification: _toggleNotification,
+                                message: "¡eliminado!",
+                                icon: Icons.remove,
+                              ),
+                            ],
+                          ),
+                        ),
+                        HorizontalCardSection(items: hcItems,), // Sección nueva
+                        CardSection(items: cItems),
+                        CardSection(items: cItems),
+                        CardSection(items: cItems),
+                        CardSection(items: cItems),
+                        CardSection(items: cItems),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
+            // Mostrar la notificación si _showNotification es verdadero
+            if (_showNotification)
+              NotificationWidget(
+                message: _notificationMessage,
+              ),
           ],
         ),
       ),
