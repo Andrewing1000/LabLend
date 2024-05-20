3624f19265618f811f5815f02aaf70e8417beae5
Mauricio <alan.fernandez@ucb.edu.bo>
2024-05-19 20:54:00 -0400

    Creacion carpeta de Pages_views

--- frontend/lib/widgets/previews/navbar_test.dart
+++ frontend/lib/widgets/previews/navbar_test.dart
@@ -1,57 +1,58 @@
-
 import 'package:flutter/material.dart';
 import '../navbar.dart';
 
-
-void main(){
-
-
+void main() {
   final List<NavItem> items = [
-
     NavItem(
         iconNormal: Icons.home_outlined,
         iconSelected: Icons.home,
-        onPressed: (){
+        onPressed: () {
           print("Opcion1");
         },
         title: "Home"),
-
     NavItem(
-      iconNormal : Icons.search_sharp,
-      iconSelected: Icons.search,
-        onPressed: (){
+        iconNormal: Icons.search_sharp,
+        iconSelected: Icons.search,
+        onPressed: () {
           print("Opcion2");
         },
-      title: "Search"),
-
+        title: "Search"),
     NavItem(
-      iconNormal : Icons.notifications_none_outlined,
-      iconSelected: Icons.notifications,
-        onPressed: (){
+        iconNormal: Icons.notifications_none_outlined,
+        iconSelected: Icons.notifications,
+        onPressed: () {
           print("Opcion3");
         },
-      title: "Notifications"),
-
+        title: "Notifications"),
     NavItem(
-      iconNormal : Icons.person_2_outlined,
-      iconSelected: Icons.person_2,
-        onPressed: (){
+        iconNormal: Icons.person_2_outlined,
+        iconSelected: Icons.person_2,
+        onPressed: () {
           print("Opcion4");
         },
-      title: "Profile"),
+        title: "Profile"),
+    NavItem(
+        iconNormal: Icons.person_2_outlined,
+        iconSelected: Icons.person_2,
+        onPressed: () {
+          print("Opcion4");
+        },
+        title: "Profile"),
   ];
 
   return runApp(MaterialApp(
-    home: Container(
-      color: Colors.orange,
-      child: Column(
-
-        children: [
-          SizedBox(height: 40,),
-
-          VerticalNavbar(items: items, iconSize: 40,),
-        ],
-      ),
-    )
-  ));
+      home: Container(
+    color: Colors.orange,
+    child: Column(
+      children: [
+        SizedBox(
+          height: 40,
+        ),
+        VerticalNavbar(
+          items: items,
+          iconSize: 40,
+        ),
+      ],
+    ),
+  )));
 }
