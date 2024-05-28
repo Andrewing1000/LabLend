8d38f32173f3ffbd3c8438ddf72e0b20b111cf5d
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-05-28 16:04:46 -0400

    creacion de usuario
    creacion de usarios con codigo comentado

---
+++ frontend/lib/screens/previews/create_user_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/create_user.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CreateUserScreen(),
+  ));
+}
---
+++ frontend/lib/widgets/user_form.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+// import 'package:frontend/models/user.dart'; // Comentado para ver solo la vista
+import 'package:frontend/widgets/boton_agregar.dart';
+import 'package:frontend/widgets/string_field.dart';
+import 'package:frontend/widgets/password_creation_field.dart';
+
+class UserForm extends StatefulWidget {
+  final Function(String, String, String, /*Role,*/ bool) onFormSubmit;
+
+  const UserForm({
+    Key? key,
+    required this.onFormSubmit,
+  }) : super(key: key);
+
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  // final List<Role> roles = [Role.adminRole, Role.assistantRole]; // Comentado para ver solo la vista
+  // Role selectedRole = Role.assistantRole; // Comentado para ver solo la vista
+  bool isActive = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        StringField(
+          controller: emailController,
+          hintText: 'Correo Electrónico',
+          width: MediaQuery.of(context).size.width * 0.8,
+        ),
+        SizedBox(height: 20),
+        StringField(
+          controller: nameController,
+          hintText: 'Nombre',
+          width: MediaQuery.of(context).size.width * 0.8,
+        ),
+        SizedBox(height: 20),
+        PasswordCreationField(
+          controller: passwordController,
+          hintText: 'Contraseña',
+          width: MediaQuery.of(context).size.width * 0.8,
+        ),
+        SizedBox(height: 20),
+        // DropdownButton<Role>(
+        //   value: selectedRole,
+        //   dropdownColor: Colors.grey[900],
+        //   style: TextStyle(color: Colors.white),
+        //   items: roles.map((Role role) {
+        //     return DropdownMenuItem<Role>(
+        //       value: role,
+        //       child: Text(
+        //         role.name,
+        //         style: TextStyle(color: Colors.white),
+        //       ),
+        //     );
+        //   }).toList(),
+        //   onChanged: (Role? newRole) {
+        //     setState(() {
+        //       selectedRole = newRole!;
+        //     });
+        //   },
+        // ),
+        // SizedBox(height: 20),
+        Row(
+          children: [
+            Checkbox(
+              value: isActive,
+              onChanged: (bool? value) {
+                setState(() {
+                  isActive = value!;
+                });
+              },
+              checkColor: Colors.white,
+              activeColor: Colors.green,
+            ),
+            Text(
+              'Activo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        BotonAgregar(
+          onPressed: () {
+            widget.onFormSubmit(
+              emailController.text,
+              nameController.text,
+              passwordController.text,
+              // selectedRole, // Comentado para ver solo la vista
+              isActive,
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
