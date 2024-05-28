485a4bcc8bf7e3dd0249986bebe68d304f8ca1b9
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-05-28 13:44:50 -0400

    creacion_item

---
+++ frontend/lib/screens/previews/create_item_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/create_item.dart'; // Asegúrate de que la ruta es correcta
+
+void main() {
+  runApp(MaterialApp(
+    home: CreationItemScreen(),
+  ));
+}
